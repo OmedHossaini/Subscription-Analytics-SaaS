dffe13aab45748bc3a4509cc0736ec8e619f7bb9
Omed <omed_hossaini@outlook.com>
2025-02-10 23:41:14 -0500

    changed the app.js to show login and signup

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import LoginPage from './components/LoginPage';
+import SignUpPage from './components/SignUpPage';
+import DashboardPage from './components/DashboardPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/login" component={LoginPage} />
+        <Route path="/signup" component={SignUpPage} />
+        <Route path="/dashboard" component={DashboardPage} />
+        <Route path="/" exact>
+          <h1>Welcome to Subscription Analytics SaaS</h1>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
