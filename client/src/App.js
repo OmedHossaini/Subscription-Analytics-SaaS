3f58c5398b60bd5620ebda2c3e86a4d146afa98f
Omed <omed_hossaini@outlook.com>
2025-02-14 18:18:15 -0500

    fixed app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LoginPage from './components/LoginPage';
 import SignUpPage from './components/SignUpPage';
 import DashboardPage from './components/DashboardPage';
@@ -7,14 +7,12 @@ import DashboardPage from './components/DashboardPage';
 function App() {
   return (
     <Router>
-      <Switch>
-        <Route path="/login" component={LoginPage} />
-        <Route path="/signup" component={SignUpPage} />
-        <Route path="/dashboard" component={DashboardPage} />
-        <Route path="/" exact>
-          <h1>Welcome to Subscription Analytics SaaS</h1>
-        </Route>
-      </Switch>
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/dashboard" element={<DashboardPage />} />
+        <Route path="/" element={<h1>Welcome to Subscription Analytics SaaS</h1>} />
+      </Routes>
     </Router>
   );
 }
